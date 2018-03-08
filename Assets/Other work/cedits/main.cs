cf229187337045aeb42c132c1aec549a0045fcd5
ahmedsherif11 <37171244+ahmedsherif11@users.noreply.github.com>
2018-03-08 14:16:01 +0200

    new track, AI , basic car texturing , basic road texturing ,cedits , main menu ,and intro
    PS all : to start enter otherworks>credits >intro scene and play , in the menue press high way for credits , press toun for game
    
    3abra7man : check 2l menue and the UI and update what you think that it would loock better ;)
    
    sherif salama : kindly find the car and the AI car zabat fyhom 2l acceleration we 2l 3arabya bt7def ymyyn we shemal kol shwaya shoof law hat3raf tzabatha kman ^_^

---
+++ Assets/Other work/cedits/main.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class main : MonoBehaviour
+{
+
+    // Use this for initialization
+    public void Startcity()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void Startroad()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+}
