0c2834b4a12176d1de0656e0515b7c94222801da
ahmedsherif11 <37171244+ahmedsherif11@users.noreply.github.com>
2018-03-10 22:38:00 +0200

    MR Ahmed Saleh
    Assets/otherwork/EVP5

---
+++ Assets/Other work/EVP5/Scripts/VehicleViewConfig.cs
@@ -0,0 +1,34 @@
+﻿/*
+  Ahmed Tolba, 2018
+  
+*/
+
+using UnityEngine;
+
+namespace EVP
+{
+
+public class VehicleViewConfig : MonoBehaviour
+	{
+	public Transform lookAtPoint;
+	public Transform driverView;
+
+	public float viewDistance = 10.0f;
+	public float viewHeight = 3.5f;
+	public float viewDamping = 3.0f;
+	public float viewMinDistance = 3.8f;
+	public float viewMinHeight = 0.0f;
+
+	public float targetDiameter { get; private set; }
+
+
+	void Awake ()
+		{
+		Bounds b = new Bounds(transform.position, Vector3.zero);
+		foreach (Renderer r in GetComponentsInChildren<Renderer>())
+			b.Encapsulate(r.bounds);
+
+		targetDiameter = (b.size.x + b.size.y + b.size.z) / 3.0f;
+		}
+	}
+}
